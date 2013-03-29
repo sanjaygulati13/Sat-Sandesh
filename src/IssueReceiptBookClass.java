a239ef3d33b86b9654d60827e4a52f3ee4feaa99
Sanjay Gulati <sanjaygulati13@gmail.com>
2013-03-29 17:02:36 +0530

    added miglayout to the new file, testing it, lets see f it gets approved

--- src/IssueReceiptBookClass.java
+++ src/IssueReceiptBookClass.java
@@ -9,7 +9,9 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
+import net.miginfocom.swing.MigLayout;
 
 
 /**
@@ -25,6 +27,7 @@ public class IssueReceiptBookClass implements ActionListener {
     TextFieldWithLimit issueDateMonthtext, issueDateYearText, issueDatetext, fromText, toText, issuedByText;
     JComboBox seriesDropDown, bookNumDropDown, stallDropDown;
     JButton okButton, cancelButton;
+    MigLayout mLayout= new MigLayout( "insets 30");
     
     Object [] stalls = {"Kirpl Bagh", "Kirpal Ashram"};
     
@@ -32,7 +35,7 @@ public class IssueReceiptBookClass implements ActionListener {
     {
         //setting environment for the Frame issueReceiptBookWindow
         issueReceiptBookWindow = new JFrame("Issue a Receipt Book");
-        issueReceiptBookWindow.setLayout(null);
+        issueReceiptBookWindow.setLayout(mLayout);
         issueReceiptBookWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         issueReceiptBookWindow.addWindowListener(new WindowAdapter() {
                 @Override
@@ -41,7 +44,7 @@ public class IssueReceiptBookClass implements ActionListener {
                 }
             }
         );
-        issueReceiptBookWindow.setSize(500,200);
+        issueReceiptBookWindow.setSize(500,300);
         //Getting the size of the screen, so that the window can 
         // adjust itself at the center of the screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -79,42 +82,49 @@ public class IssueReceiptBookClass implements ActionListener {
         stallDropDown = new JComboBox(stalls);
         
         //TextFields
+        issueDatetext = new TextFieldWithLimit( 2 , 2 );
         issueDateMonthtext = new TextFieldWithLimit( 2 , 2 );
         issueDateYearText = new TextFieldWithLimit( 4 , 4 );
-        issueDatetext = new TextFieldWithLimit( 2 , 2 );
+
         fromText = new TextFieldWithLimit( 5 , 5 );
+        fromText.setEnabled(false);
+        
         toText  = new TextFieldWithLimit( 5 , 5 );
-        issuedByText = new TextFieldWithLimit(32, 32);
+        toText.setEnabled(false);
+        
+        issuedByText = new TextFieldWithLimit(25, 25);
         
         
         //Buttons
         okButton = new JButton("OK");
+        okButton.setMnemonic('o');
         okButton.addActionListener(this);
         
         cancelButton = new JButton("Cancel");
+        cancelButton.setMnemonic('c');
         cancelButton.addActionListener(this);
         
         
         
-        //adding the gui elements to frame
+        //adding the gui elements to frame and setting the layout simultaneously
         issueReceiptBookWindow.add(seriesLabel);
-        issueReceiptBookWindow.add(seriesDropDown);
+        issueReceiptBookWindow.add(seriesDropDown, "span 2, w 100!");
         issueReceiptBookWindow.add(bookNumLabel);
-        issueReceiptBookWindow.add(bookNumDropDown);
-        issueReceiptBookWindow.add(fromLabel);
-        issueReceiptBookWindow.add(fromText);
+        issueReceiptBookWindow.add(bookNumDropDown,"span 3, w :90: , wrap 20px");
+        issueReceiptBookWindow.add(fromLabel );
+        issueReceiptBookWindow.add(fromText, "span 2, w 100!");
         issueReceiptBookWindow.add(toLabel);
-        issueReceiptBookWindow.add(toText);
+        issueReceiptBookWindow.add(toText, "span 3, w :90:, wrap 20px");
         issueReceiptBookWindow.add(issuedToLabel);
-        issueReceiptBookWindow.add(stallDropDown);
+        issueReceiptBookWindow.add(stallDropDown, "span 2, w 100!");
         issueReceiptBookWindow.add(issueDateLabel);
-        issueReceiptBookWindow.add(issueDatetext);
-        issueReceiptBookWindow.add(issueDateMonthtext);
-        issueReceiptBookWindow.add(issueDateYearText);
+        issueReceiptBookWindow.add(issueDatetext, "split 3, w 20!");
+        issueReceiptBookWindow.add(issueDateMonthtext, " w 20!");
+        issueReceiptBookWindow.add(issueDateYearText, "w 40! , wrap 20px");
         issueReceiptBookWindow.add(issuedByLabel);
-        issueReceiptBookWindow.add(issuedByText);
-        issueReceiptBookWindow.add(okButton);
-        issueReceiptBookWindow.add(cancelButton);
+        issueReceiptBookWindow.add(issuedByText,"wrap 20px");
+        issueReceiptBookWindow.add(okButton, "gapleft 80, w :90:, span 2");
+        issueReceiptBookWindow.add(cancelButton, "gapleft 60, w :90: ,span 3");
         
     
         //this is the last statement in the constructor, to make the frame visible
@@ -125,7 +135,44 @@ public class IssueReceiptBookClass implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent event)
     {
+        if(event.getSource() == okButton)
+        {
+            //gather the info from user input into the frame
+            /*
+            String seriesName = seriesText.getText();
+            String receiptString = (String)(receiptNumComboBox.getSelectedItem());
+            String bookletString = bookletNumText.getText();
+            String startingString = startingReceiptNumText.getText();
+            if(!seriesName.isEmpty() && !receiptString.isEmpty() && !bookletString.isEmpty() && !startingString.isEmpty())
+            {
+                
+                int numReceipts = Integer.parseInt(receiptString);
+                int numBooklets = Integer.parseInt(bookletString);
+                int startingNum = Integer.parseInt(startingString);*/
+                JOptionPane.showConfirmDialog(issueReceiptBookWindow, "Are you sure ?");
+                /*
+            }
+             
+             
+              else 
+                JOptionPane.showMessageDialog(issueReceiptBookWindow, "Please fill all the fields");
+             */
+            
+            
+        }
+        else if(event.getSource() == cancelButton)
+        {
+            issueReceiptBookWindow.setVisible(false);
+            new sams();
+            issueReceiptBookWindow.dispose();
+            
+        }
         
+    }
+    
+    public static void main(String args[])
+    {
+        new IssueReceiptBookClass();
     }
     
 }
